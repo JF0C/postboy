8bf9d0f3d1f2a0d3c86c94105770bf02e7300976
Jan.Chen.ext <jan.chen.ext@bestsecret.com>
2023-06-21 14:23:43 +0200

    cleanup

--- Postboy/Helpers/ContentConversion.cs
+++ Postboy/Helpers/ContentConversion.cs
@@ -6,13 +6,29 @@ namespace Postboy.Helpers
     {
         public static FormUrlEncodedContent StringToFormContent(string content)
         {
+            return new FormUrlEncodedContent(StringToKeyValuePairs(content).ToArray());
+        }
+
+        public static List<KeyValuePair<string, string>> StringToKeyValuePairs (string content)
+        {
             var nameValueCollection = HttpUtility.ParseQueryString(content);
             var keyValuePairs = new List<KeyValuePair<string, string>>();
             foreach (string key in nameValueCollection)
             {
                 keyValuePairs.Add(new KeyValuePair<string, string>(key, nameValueCollection[key]));
             }
-            return new FormUrlEncodedContent(keyValuePairs.ToArray());
+            return keyValuePairs;
+        }
+
+        public static string KeyValuePairsToUrlEncodedString(IEnumerable<KeyValuePair<string, string>> keyValuePairs)
+        {
+            var result = string.Empty;
+            foreach(var kv in keyValuePairs)
+            {
+                result += HttpUtility.UrlEncode(kv.Key) + "=" + HttpUtility.UrlEncode(kv.Value) + "&";
+            }
+            result.TrimEnd('&');
+            return result;
         }
     }
 }
